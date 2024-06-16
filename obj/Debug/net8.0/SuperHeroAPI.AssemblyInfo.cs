0bdaf836d6dd4a1eeb2ff9dea456d158184c5199
huzail <huzail2000@gmail.com>
2024-06-16 22:57:49 +0500

    Created a Dot NET Core WEB API Application

--- obj/Debug/net8.0/SuperHeroAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SuperHeroAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperHeroAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a94d284ae1c167ea10c2f90b39e658e50e9e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperHeroAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperHeroAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
